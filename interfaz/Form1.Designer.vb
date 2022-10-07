b7971b2b5dd4eb600720a9dada171165e61b345f
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-10-07 15:08:05 -0600

    Inicio del Proyecto

--- interfaz/Form1.Designer.vb
+++ interfaz/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Principal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,10 +22,18 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Name = "Principal"
+        Me.ShowIcon = False
+        Me.Text = "Expediente Médico"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
