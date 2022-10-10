fc84f17ff5df1b0601c30d05dc062895bc7373c7
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-10-10 13:31:05 -0600

    enviado

--- interfaz/Form1.Designer.vb
+++ interfaz/Form1.Designer.vb
@@ -22,18 +22,31 @@ Partial Class Principal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(437, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ejemplo para susan"
         '
         'Principal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Principal"
         Me.ShowIcon = False
         Me.Text = "Expediente Médico"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Label1 As Label
 End Class
